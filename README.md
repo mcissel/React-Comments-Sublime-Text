7f8f5152f94346953584a524a7e02c036dc2927b
mcissel <mcissel@gmail.com>
2017-04-05 16:10:43 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # React-Comments-Sublime-Text
 Very basic comments for JSX HTML tags
+
+Install
+============
+
+Navigate to your packages folder (~user\AppData\Roaming\Sublime Text 3\Packages) and clone.
+
+`git clone https://github.com/mcissel/React-Comments-Sublime-Text.git React\ Comments`
+
+
+Usage
+============
+
+Toggle on and off the `{/*` and `*/}` style JSX comments. Pretty basic.
+
+
+Keybindings
+============
+
+This uses the standard comment keybinding `ctrl+/` in Windows and Linux, `super+forward_slash` in OSX.
+
+I haven't tested it in OSX or Linux, so let me know if it's not working. Meanwhile, you can map the `react_comments` command to a different key combo if it's not working for you.
---
+++ reactComments.py
@@ -0,0 +1,38 @@
+import sublime, sublime_plugin
+
+class ReactCommentsCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    for region in self.view.sel():
+
+      someUncommented = False
+      lines = self.view.lines(region)
+      for line in lines:
+        if not isCommentLine(self, line):
+          someUncommented = True
+
+
+      if someUncommented:
+        for line in reversed(lines):
+          if not isCommentLine(self, line):
+            addComments(self, edit, line)
+      else:
+        removeComments(self, edit, self.view.line(region))
+
+
+def isCommentLine(self, line):
+  if "comment.block.js" in self.view.scope_name(line.begin()):
+    return True
+
+  lineStr = self.view.substr(line)
+  return "{/*" in lineStr or "*/}" in lineStr
+
+
+def addComments(self, edit, line):
+  self.view.insert(edit, line.end(), " */}")
+  self.view.insert(edit, line.begin(), "{/* ")
+
+
+def removeComments(self, edit, region):
+  regionStr = self.view.substr(region)
+  uncommented = regionStr.replace("{/* ", "").replace("*/}", "")
+  self.view.replace(edit, region, uncommented)
